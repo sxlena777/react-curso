2ebb14574422d2a0cef0f060126ee6dd36b323ad
Lautaro Robales <lautarorobales31@gmail.com>
2023-04-25 00:21:38 -0300

    customizacion de index y source

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const rootElement = document.getElementById('root') //elemento root seleccionado
+const root = ReactDOM.createRoot(rootElement)
+
+
+root.render(<h1>Hola mundo<h1/>)
+
