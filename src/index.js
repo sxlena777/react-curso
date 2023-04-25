85e5204bd5aba38a53889564b7b92d0a90ff7f3f
Lautaro Robales <lautarorobales31@gmail.com>
2023-04-25 20:58:32 -0300

    Modificando src/index.js y comp.jsx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import { CompEjemplo } from './comp'
+import ReactDOM from "react-dom/client";
+import { CompEjemplo, ComponenteCard } from "./comp";
 
-const rootElement = document.getElementById('root') //elemento root seleccionado
-const root = ReactDOM.createRoot(rootElement)
-
-
+const rootElement = document.getElementById("root"); //elemento root seleccionado
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <>
-        <CompEjemplo />
-        <CompEjemplo />
-        <CompEjemplo />
-    </>
-)
+  <>
+    <CompEjemplo title="Hola 1" />
+
+    <ComponenteCard
+      nombre="lautaro"
+      nick="zoso"
+      edad={30}
+      casado={true}
+      direccion={{
+        calle: "street example",
+        altura: 123,
+        edificio: false, // false -> casa | true -> Departamento
+      }}
+    />
+  </> // cierre de fake tag
+);
