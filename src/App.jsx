8c9d35fa185be94786a22409ca8ed9b11894d44c
Lautaro Robales <lautarorobales31@gmail.com>
2023-04-29 21:27:14 -0300

    instanciando tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+
+export const Square = () => {
+    return(
+        <button className='Square'>X</button>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CompEjemplo, ComponenteCard } from "./comp";
-import { Boton } from "./Boton";
 
-//import { Boton } from "./Boton";
+import {Square} from './App';
 
-const rootElement = document.getElementById("root"); //elemento root seleccionado
-const root = ReactDOM.createRoot(rootElement);
+const getRoot = document.getElementById("root");
 
-const change = (info) => {
-  // Muestra por pantalla cuando se ejecute un evento de cambio en el button, muestra el valor de ese button en tiempo real por consola
-  console.clear();
-  console.log(info.target.value);
-};
+const root = ReactDOM.createRoot(getRoot);
 
 root.render(
-  <>
-    <CompEjemplo title="Hola 1" />
+<>
 
-    <ComponenteCard
-      nombre="lautaro"
-      nick="zoso"
-      edad={30}
-      casado={true}
-      direccion={{
-        calle: "street example",
-        altura: 123,
-        edificio: false, // false -> casa | true -> Departamento
-      }}
-    />
-
-    <ComponenteCard
-      nombre="name"
-      nick="asdfqwer"
-      edad={44}
-      casado={false}
-      direccion={{
-        calle: "calle de ejemplo",
-        altura: 189,
-        edificio: true, // false -> casa | true -> Departamento
-      }}
-    />
+    <Square />
 
 
-    <Boton />
-
-    <input onChange={change}></input>
-
-  </> // cierre de fake tag
-);
+</>);
